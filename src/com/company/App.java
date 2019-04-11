43d483cad427bb5f4f80c9d45017eea61d4ee0c3
Bosco <boscodomingob@gmail.com>
2019-04-11 18:56:28 +0200

    Random mierdas

---
+++ src/com/company/App.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class App {
+    public static void run(){
+        System.out.println("Bienvenido a TwitterFIS, por favor seleccione una opción:");
+        int selector = Scanner.
+        switch (selector){
+        
+        }
+        User user1 = new User();
+        
+    }
+}
