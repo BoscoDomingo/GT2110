18f7b43741feb4bb9bd2e4c63dd09b7ebe0362fa
Bosco <boscodomingob@gmail.com>
2019-05-13 00:34:38 +0200

    Añadido ejemplo menu en IMenu
    Copiad y pegad de IMenu al lugar que necesitéis. En principio tendría que funcionar

--- src/interfaces/IMenu.java
+++ src/interfaces/IMenu.java
@@ -2,7 +2,37 @@ package interfaces;
 
 public interface IMenu {
 
-    public void menu(int ultimoNumero); //se llama a todos los menus en una pantalla (ejemplo: estamos en el perfil
-    // de un usuario, se llama a perfil.menu(0) que dentro llamará a timeline.menu(5), suponiendo que las opciones
-    // del 0 al 4 están cogidas por acciones del perfil, y del 5 al 8, de la timeline
+    public boolean menu(); //se llama a todos los menus en una pantalla (ejemplo: estamos en el perfil
+    // de un usuario, se llama a perfil.menu() que dentro tendrá una opción que llamará a timeline.menu()
+    //EJEMPLO DE UN MENÚ
+   /*
+    boolean accionValida = false, goBack = false;
+    Scanner scan = new Scanner(System.in);
+    while (!accionValida) {
+        int selector = scan.nextInt();
+        switch (selector) {
+        //METED CASES SÓLO DEL 0 AL 8
+            case 0:
+                accionValida = true;
+                //vuestro código aqui
+                break;
+
+                //...
+            case 8:
+                accionValida = true;
+                //LLAMADA A CUALQUIER OTRO MENU QUE SE PUEDA MOSTRAR SI LO NECEISTAIS, INCLUIDAS MÁS ACCIONES SI 8 NO
+                //SON SUFICIENTES
+                if(otraCosa.menu() = true) accionValida = false; //Significa que vuelve del otro menu a este
+                break;
+            case 9:
+                accionValida = true;
+                goBack = true;
+                break;
+            default:
+                System.out.println("Por favor, introduzca un número válido (del 0 al ");//VUESTRO NUMERO.SI HAY 3
+                //OPCIONES, PONÉIS EL 2
+        }
+    }
+    return goBack; //Si es true, se vuelve a mostrar el menú que haya llamado a este
+    */
 }
