20cd4654a4de97dca971128473a02ed8e0bc4daa
Luis <luisdemarcos1999@gmail.com>
2019-05-16 17:21:47 +0200

    Publicacion añadir

--- src/main/java/pkg/App.java
+++ src/main/java/pkg/App.java
@@ -1,31 +1,46 @@
-package pkg;
+package main.java.pkg;
 
 import controllers.UserController;
+import main.java.publicaciones.Publicacion;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void run() {
         UserController userController = new UserController();
+        ArrayList<Publicacion> allPublicaciones = new ArrayList<>();
+
         System.out.println("Bienvenido a TwitterFIS, por favor seleccione una opción:");
-        System.out.println("1 - Crear nuevo usuario\n2 - Iniciar sesión");
+        System.out.println("1 - Crear nuevo usuario\n2 - Iniciar sesión\n3 - Crear publicacion");
         Scanner scan = new Scanner(System.in);
         int selector = scan.nextInt();
-        switch (selector) {
-            case 1:
-                String email = scan.nextLine();
-                String password = scan.nextLine();
-                userController.crearCuenta(email, password);
-                break;
-            case 2:
-                email = scan.nextLine();
-                password = scan.nextLine();
-                userController.iniciarSesion(email, password);
-                break;
-                
-            default:
-                System.out.println("Por favor, introduzca un número válido");
-            
+        while(selector != 0) {
+            switch (selector) {
+                case 1:
+                    String email = scan.nextLine();
+                    String password = scan.nextLine();
+                    userController.crearCuenta(email, password);
+                    break;
+                case 2:
+                    email = scan.nextLine();
+                    password = scan.nextLine();
+                    userController.iniciarSesion(email, password);
+                    break;
+                case 3:
+                    allPublicaciones.add(new Publicacion().addPublicacion(allPublicaciones));
+                    break;
+                case 4:
+                    for(int i=0; i<allPublicaciones.size(); i++){
+                        allPublicaciones.get(i).show();
+                    }
+                    break;
+
+                default:
+                    System.out.println("Por favor, introduzca un número válido");
+
+            }
+            selector = scan.nextInt();
         }
     }
 }
--- src/main/java/publicaciones/Publicacion.java
+++ src/main/java/publicaciones/Publicacion.java
@@ -1,4 +1,4 @@
-package publicaciones;
+package main.java.publicaciones;
 
 import pkg.CuentaUsuario;
 import pkg.Timeline;
@@ -7,6 +7,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Publicacion {
     private final String id;
@@ -18,6 +19,13 @@ public class Publicacion {
     private ArrayList<Valoracion> valoraciones;
     private ArrayList<Comentario> comentarios;
     private ArrayList<Timeline> perteneceATimelines;
+
+    public Publicacion(){
+        id = null;
+        poster = null;
+        fecha = null;
+        texto = null;
+    }
 
     public Publicacion(String id, CuentaUsuario poster, String texto, ArrayList<Valoracion> valoraciones,
                        ArrayList<Comentario> comentarios, ArrayList<Timeline> perteneceATimelines) {
@@ -35,12 +43,55 @@ public class Publicacion {
     public void show() {
         DateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         System.out.println("_____________________________________");
-        System.out.println("Publicacion de " + this.poster.getAlias() + ":");
+        //System.out.println("Publicacion de " + this.poster.getAlias() + ":");
         System.out.println("\n" + this.texto);
         System.out.println("\n" + formatoFecha.format(fecha));
         System.out.println("Likes: " + this.numeroLikes + " Dislikes: " + this.numeroDislikes);
         System.out.println("Comentarios: " + this.comentarios.size());
         System.out.println("_____________________________________");
+    }
+
+    public Publicacion addPublicacion(ArrayList<Publicacion> allPublicaciones) {
+        System.out.println("Eliga el tipo de publicacion que desea realizar: ");
+        System.out.println("1 - Texto\n2 - Enlace\n3 - Referencia");
+        Scanner scan = new Scanner(System.in);
+        int selector = scan.nextInt();
+        switch (selector) {
+            case 1:
+                 return addPublicacionTexto(allPublicaciones);
+            case 2:
+                return addPublicacionEnlace(allPublicaciones);
+            case 3:
+                return addPublicacionReferencia(allPublicaciones);
+        }
+        return null;
+    }
+
+    private Publicacion addPublicacionTexto(ArrayList<Publicacion> allPublicaciones) {
+            Scanner scan = new Scanner(System.in);
+            String texto = null;
+        do {
+            System.out.println("Escriba el contenido a continuacion: "); //EVITAR TEXTO VACIO
+            texto = scan.nextLine();
+        }while(texto.length()==0);
+        
+        String id = "b0000";
+        if(allPublicaciones.size()!=0) {
+            id = allPublicaciones.get(allPublicaciones.size() - 1).getId();
+            id = id.substring(1,5);
+            id = "b" + String.format("%04d", Integer.parseInt(id)+1);           //El pinche numero del id junto a la b
+        }
+        System.out.println(id);
+        return new Publicacion(id,null,texto,new ArrayList<Valoracion>(),new ArrayList<Comentario>(),new ArrayList<Timeline>());
+    }
+
+    private Publicacion addPublicacionEnlace(ArrayList<Publicacion> allPublicaciones) {
+        System.out.println("Hola");
+        return null;
+    }
+
+    private Publicacion addPublicacionReferencia(ArrayList<Publicacion> allPublicaciones) {
+        return null;
     }
 
     /*public void deleteComentario(String idComentario) {
@@ -75,4 +126,8 @@ public class Publicacion {
     public int getNumeroLikes() {
         return numeroLikes;
     }
+    public String getId() {
+        return id;
+    }
+
 }
