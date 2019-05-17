9702a99234d52ffdf82fce71afce0906944ffdeb
Bosco <boscodomingob@gmail.com>
2019-05-17 12:13:28 +0200

    Cambios varios
    Añadidas las versiones de 1 solo elemento y de ArrayList de los metodos en CuentaUsuario.
    Añadidos try...catch de ejemplo
    Añadido TimelineTest y cambios a PublicacionTest

---
+++ src/test/java/pkg/TimelineTest.java
@@ -0,0 +1,30 @@
+package pkg;
+
+import org.junit.Test;
+import publicaciones.Publicacion;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class TimelineTest {
+
+    @Test
+    public void showTimelineByPages() {
+        ArrayList<Publicacion> publicacions = new ArrayList<>();
+        for (int i = 0; i < 101; i++) {
+            publicacions.add(new Publicacion("b00" + i,
+                                             new CuentaUsuario("a00" + i, "" + i, i + "@upm.es", null, null, null,
+                                                               null, null), "Numero: " + i, null, new ArrayList<>(),
+                                             null));
+        }
+        Timeline timeline = new Timeline(publicacions);
+        int currentPage, numeroDePaginas = 5;
+        for (currentPage = 1; currentPage < numeroDePaginas + 1; currentPage++) {
+            timeline.showPage(currentPage);
+            String input = "0";
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+        }
+    }
+}
