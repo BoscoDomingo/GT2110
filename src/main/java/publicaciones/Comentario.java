713c5b0a7e3cf818112c00a819776914214f57a0
M <miguel.roca.garcia@gmail.com>
2019-05-22 12:05:55 +0200

    Menú de un comentario y sus respuestas
    Muestra el comentario con sus respuestas y da la  opción de responder un comentario o una respuesta

--- src/main/java/publicaciones/Comentario.java
+++ src/main/java/publicaciones/Comentario.java
@@ -1,11 +1,14 @@
 package publicaciones;
 
+import interfaces.IMenu;
 import pkg.CuentaUsuario;
+import pkg.Sistema;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 
-public class Comentario {
+public class Comentario implements IMenu {
     private final String id;
     private Date fecha;
     private String texto;
@@ -58,6 +61,60 @@ public class Comentario {
 
     public CuentaUsuario getEscritoPor() {
         return escritoPor;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void mostrarComentarioYRespuestas(){
+        System.out.println("@" + this.escritoPor.getAlias() + "\t" + this.fecha);
+        System.out.println(this.texto);
+        for(Comentario respuesta: this.respuestas){
+            System.out.println("\t@" + respuesta.escritoPor.getAlias() + "\t" + respuesta.getFecha());
+            System.out.println("\t Responde a: @" + respuesta.getRespondeA().getEscritoPor().getAlias() + ". " + respuesta.texto);
+        }
+    }
+
+    public Comentario elegirComentario(){
+        System.out.println("Elegir el comentario a responder introduciendo su posición: ");
+        System.out.println("(0 para responder el comentario principal y 1 para la primera respuesta, etc.)");
+        Scanner scan = new Scanner(System.in);
+        int eleccion = scan.nextInt();
+        if (eleccion == 0)
+            return this;
+        else if (eleccion <= this.respuestas.size())
+            return this.respuestas.get(eleccion - 1);
+        else {
+            System.out.println("Número introducido no válido");
+            return this.elegirComentario();
+        }
+    }
+
+    @Override
+    public boolean menu() {
+        this.mostrarComentarioYRespuestas();
+        System.out.println("\n1 - Responder comentario \n9 - Atrás\n");
+        Scanner scan = new Scanner(System.in);
+        int selector = scan.nextInt();
+        boolean valido = false;
+        while (!valido) {
+            switch (selector){
+                case 1:
+                    valido = true;
+                    Comentario comentario = this.elegirComentario();
+                    System.out.printf("Texto: ");
+                    String texto = scan.nextLine();
+                    Sistema.getCurrentUser().comentarComentario(comentario,texto);
+                    break;
+                case 9:
+                    valido = true;
+                    break;
+                default:
+                    System.out.println("Valor no válido");
+            }
+        }
+        return true;
     }
 }
 
